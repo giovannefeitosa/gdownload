9b92d9101e9d19c8fafcff22244315b79d7e0e1d
Giovanne <giovanne.afonso@opah.com.br>
2019-08-14 14:39:49 -0300

    Download from chrome OK

--- gdownload.go
+++ gdownload.go
@@ -1,38 +1,50 @@
 package main
 
+/////////////////////////////////////////////////////////////////
+// Require installing the following packages
+// - youtube-dl
+// - ffmpeg
+/////////////////////////////////////////////////////////////////
+
 import (
 	"fmt"
 	"log"
-	"os"
 	"os/exec"
 	"strings"
-
-	"github.com/rylio/ytdl"
 )
 
 func main() {
+
+	fmt.Println("")
+	fmt.Println("gdownload")
+	fmt.Println("  Download mp3 from currently playing youtube video on Google Chrome")
+	fmt.Println("  by giovanneafonso@gmail.com")
+	fmt.Println("")
+
 	outputPath := "/home/opah/Música"
 	videoID := getChromePlayingYoutubeID()
 	log.Printf("videoID: %v", videoID)
-	download("https://www.youtube.com/watch?v="+videoID, outputPath)
+	download(videoID, outputPath)
 }
 
 // Receives a url and outputs a file with the same name
-func download(videoUrl, outputPath string) {
-	vid, err := ytdl.GetVideoInfo(videoUrl)
+func download(videoID, outputPath string) {
+	filePath := fmt.Sprintf("%s/%s", outputPath, "%(title)s-%(id)s.%(ext)s")
+	cmdString := fmt.Sprintf(
+		"youtube-dl --extract-audio --audio-format mp3 -o \"%s\" https://youtube.com/watch?v=%s",
+		filePath,
+		videoID)
+
+	cmd := exec.Command("bash", "-c", cmdString)
+	out, err := cmd.Output()
+
 	if err != nil {
-		log.Fatalf("Failed to get video info: %v", err)
-		return
+		log.Fatalf("Unable to execute youtube-dl command: \n-Cmd: %s\n-Error: %v", cmdString, err)
+	} else {
+		log.Fatalf("Out: %s", out)
 	}
-	filePath := fmt.Sprintf("%s/%s.mp4", outputPath, escapeVideoTitle(vid.Title))
-	log.Printf("filePath: %v", filePath)
-	file, err := os.Create(filePath)
-	if err != nil {
-		log.Fatalf("Failed to create file: %v", err)
-		return
-	}
-	defer file.Close()
-	vid.Download(vid.Formats[0], file)
+
+	fmt.Print(string(out))
 }
 
 // Returns the youtube video ID currently playing on chrome
@@ -60,9 +72,4 @@ func getChromePlayingYoutubeID() string {
 	}
 
 	return videoID
-}
-
-func escapeVideoTitle(videoTitle string) string {
-	videoTitle = strings.Replace(videoTitle, " ", "-", -1)
-	return videoTitle
 }
